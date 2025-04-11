556b5f510e8a8dc1d89117916faef1eb440688ab
vedikagarwal <vedikagarwal98@gmail.com>
2025-04-11 18:31:34 -0400

    Further improvements

--- my_genomics_workflow_2/work/c8/7b76de3d5d13bcc358ad119e5b87d2/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-fastp             --in1 SRR1556289_1.fastq.gz             --in2 SRR1556289_2.fastq.gz             --out1 SRR1556289_1.trimmed.fastq.gz             --out2 SRR1556289_2.trimmed.fastq.gz             --json SRR1556289.fastp.json             --html SRR1556289.fastp.html             --thread 2
